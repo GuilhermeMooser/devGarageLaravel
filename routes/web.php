de36e1329783685fab135565c527d2d35a0e596a
GuilhermeMooser <guilhermemoserm@gmail.com>
2024-10-27 15:26:48 -0300

    feature/ The Scss custom add

--- routes/web.php
+++ routes/web.php
@@ -1,21 +1,24 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
 use App\Http\Controllers\VehicleController;
-use App\Http\Controllers\VehicleTypesController;
 
-// returns the home page with all posts
-Route::get('/', VehicleController::class .'@index')->name('vehicle.index');
-// returns the form for adding a post
-Route::get('/vehicle/create', VehicleController::class . '@create')->name('vehicle.create');
-// adds a post to the database
-Route::post('/vehicle', VehicleController::class .'@store')->name('vehicle.store');
-// returns a page that shows a full post
-Route::get('/vehicle/{vehicle}', VehicleController::class .'@show')->name('vehicle.show');
-// returns the form for editing a post
-Route::get('/vehicle/{vehicle}/edit', VehicleController::class .'@edit')->name('vehicle.edit');
-// updates a post
-Route::put('/vehicle/{vehicle}', VehicleController::class .'@update')->name('vehicle.update');
-// deletes a post
-Route::delete('/vehicle/{vehicle}', VehicleController::class .'@destroy')->name('vehicle.destroy');
+Auth::routes();
 
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+// Returns the home page with all vehicles
+// Route::get('/', VehicleController::class . '@index')->name('vehicle.index');
+// Returns the form for adding a vehicle
+// Route::get('/vehicle/create', VehicleController::class . '@create')->name('vehicle.create');
+// adds a vehicle to the database
+// Route::post('/vehicle', VehicleController::class . '@store')->name('vehicle.store');
+// Returns a page that shows a full vehicle
+// Route::get('/vehicle/{vehicle}', VehicleController::class . '@show')->name('vehicle.show');
+// Returns the form for editing a vehicle
+// Route::get('/vehicle/{vehicle}/edit', VehicleController::class . '@edit')->name('vehicle.edit');
+// Updates a vehicle
+// Route::put('/vehicle/{vehicle}', VehicleController::class . '@update')->name('vehicle.update');
+// Deletes a vehicle
+// Route::delete('/vehicle/{vehicle}', VehicleController::class . '@destroy')->name('vehicle.destroy');
